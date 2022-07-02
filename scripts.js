19af023a86b526be7361c0a800c233a2a7842a8e
angelo <angelodelperez@gmail.com>
2022-07-02 00:28:53 -0500

    pass json to html

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
+
 const daysamount = () =>{
-    let amount;
     fetch("./data.json")
     .then(res =>res.ok ? Promise.resolve(res) : Promise.reject(res))
     .then(res=>res.json())
     .then(res => {
-        amount = res;
+        const week = document.getElementById('spending__week')
+        const fragment = document.createDocumentFragment()
+        for(const days of res){
+            const item = document.createElement('P')
+            item.textContent = ` day ${days.day} and amount ${days.amount}`
+            fragment.appendChild(item)
+        }
+        week.appendChild(fragment)
     })
-    console.log(amount)
+    
 }
 
 
