293fe6a7136fff3e82d387b93e824b9fb0d36fb0
angelo <angelodelperez@gmail.com>
2022-07-02 15:22:44 -0500

    finished the amount, correct font size and margins

--- scripts.js
+++ scripts.js
@@ -12,8 +12,23 @@ const daysamount = () =>{
             const amount = document.createElement('P')
             day.textContent = `${days.day}`
             amount.textContent = `${days.amount}`
+            divitem.classList.add('spending__amount')
+            amount.style.margin = "0"
+            amount.style.height = `${days.amount * 1.3}%`
+            amount.style.borderRadius = "3px" 
+            console.log(typeof(days.amount))
+            if (days.amount>=50) {
+                amount.style.backgroundColor = "hsl(186, 34%, 60%)"
+            }
+            else{
+                amount.style.backgroundColor = "hsl(10, 79%, 65%)"
+            }
+
+            day.style.marginBottom = "0"
             divitem.appendChild(amount)
             divitem.appendChild(day)
+
+            
             fragment.appendChild(divitem)
         }
         week.appendChild(fragment)
